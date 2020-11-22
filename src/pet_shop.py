78dac963aa38a7d9acf0512746ecce0aa478275f
Andrew Wood <andrewwood90@hotmail.com>
2020-11-22 12:21:41 +0000

    10 questions done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -8,3 +8,51 @@ def get_pet_shop_name(shop):
 
 def get_total_cash(shop):
     return shop["admin"]['total_cash']
+
+# 3
+
+def add_or_remove_cash(shop, cash):
+    shop["admin"]["total_cash"] += cash
+
+# 4
+
+def add_or_remove_cash(shop, cash):
+    shop["admin"]["total_cash"] += cash
+
+# 5
+
+def get_pets_sold(shop):
+    return shop["admin"]['pets_sold']
+
+# 6 
+
+def increase_pets_sold(shop, number):
+    shop["admin"]['pets_sold'] += number
+
+# 7
+
+def get_stock_count(shop):
+    return len(shop["pets"])
+
+
+# 8
+
+def get_pets_by_breed(shop, breed_desired):
+    number_of_desired_breed = []
+    for pet in shop["pets"]:
+        if pet["breed"] == breed_desired:
+            number_of_desired_breed.append(pet)
+    return number_of_desired_breed
+
+#9 is the same function
+
+# 10
+
+def find_pet_by_name(list,name):
+    for pet in list["pets"]:
+        if pet["name"] == "name":
+            return pet
+        return "Not found."
+
+# 11
+
